e3253cd5f974e1bd46941e3e0240c7ee4f8468da
Daniil Temnyj <temnyy1@gmail.com>
2024-07-12 18:28:59 +0300

    Changed project template from example to generated

--- src/client/java/com/hitchsavan/testmod/TestModClient.java
+++ src/client/java/com/hitchsavan/testmod/TestModClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package com.hitchsavan.testmod;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class TestModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
